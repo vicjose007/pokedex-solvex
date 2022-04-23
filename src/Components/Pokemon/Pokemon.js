fe33e859d8fbc30d1e799554ede242980c481174
vicjose007 <vicjose007@gmail.com>
2022-04-22 20:38:39 -0400

    added pokemon stats

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable default-case */
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
 import axios from "axios";
@@ -7,7 +9,24 @@ export default class Pokemon extends Component {
     name: "",
     imageUrl: "",
     pokemonIndex: "",
-    loading: true,
+    types: [],
+    description: " ",
+    stats: {
+      hp: " ",
+      attack: " ",
+      defense: " ",
+      speed: " ",
+      specialAttack: " ",
+      specialDefense: " ",
+    },
+
+    height: " ",
+    weight: " ",
+    abilities: " ",
+    genderRatioMale: " ",
+    genderRatioFemale: " ",
+    evs: " ",
+    hatchStep: " ",
   };
 
   async fetchData() {
@@ -19,19 +38,118 @@ export default class Pokemon extends Component {
 
     const pokemonRes = await axios.get(pokemonUrl);
     const name = pokemonRes.data.name;
-    
-    
-    
-    this.setState({ name, loading: false });
+    const imageUrl = pokemonRes.data.sprites.front_default;
+
+    let { hp, attack, defense, speed, specialAttack, specialDefense } = "";
+
+    pokemonRes.data.stats.map((stat) => {
+      switch (stat.stat.name) {
+        case "hp":
+          hp = stat["base_stat"];
+          break;
+        case "attack":
+          attack = stat["base_stat"];
+          break;
+        case "defense":
+          defense = stat["base_stat"];
+          break;
+        case "speed":
+          speed = stat["base_stat"];
+          break;
+        case "specialAttack":
+          specialAttack = stat["base_stat"];
+          break;
+        case "specialDefense":
+          specialDefense = stat["base_stat"];
+          break;
+      }
+    });
+
+    const height =
+      Math.round((pokemonRes.data.height * 0.328084 + 0.0001) * 100) / 100;
+
+    const weight =
+      Math.round((pokemonRes.data.height * 0.220462 + 0.0001) * 100) / 100;
+
+    const types = pokemonRes.data.types.map((type) => type.type.name);
+
+    const abilities = pokemonRes.data.abilities.map((ability) => {
+      return ability.ability.name
+        .split("-")
+        .map((s) => s.chartAt(0).toUpperCase() + s.substrings(1))
+        .join(" ");
+    });
+
+    const evs = pokemonRes.data.stats
+      .filter((stat) => {
+        if (stat.effort > 0) {
+          return true;
+        }
+        return false;
+      })
+      .map((stat) => {
+        return `${stat.effort} ${stat.stat.name
+          .toLowerCase()
+          .split("-")
+          .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(" ")}`;
+      })
+      .join(", ");
+
+    // Get Pokemon Description .... Is from a different end point uggh
+    this.get(pokemonSpeciesUrl).then((res) => {
+      let description = "";
+      res.data.flavor_text_entries.some((flavor) => {
+        if (flavor.language.name === "en") {
+          description = flavor.flavor_text;
+          return;
+        }
+      });
+      const femaleRate = res.data["gender_rate"];
+      const genderRatioFemale = 12.5 * femaleRate;
+      const genderRatioMale = 12.5 * (8 - femaleRate);
+
+      const catchRate = Math.round((100 / 255) * res.data["capture_rate"]);
+
+      const hatchSteps = 255 * (res.data["hatch_counter"] + 1);
+
+      this.setState({
+        description,
+        genderRatioFemale,
+        genderRatioMale,
+        catchRate,
+        hatchSteps,
+      });
+    });
+
+    this.setState({
+      imageUrl,
+      pokemonIndex,
+      name,
+      types,
+      stats: {
+        hp,
+        attack,
+        defense,
+        speed,
+        specialAttack,
+        specialDefense,
+      },
+      height,
+      weight,
+      abilities,
+      evs,
+    });
   }
 
-   componentDidMount() {
+  componentDidMount() {
     this.fetchData();
   }
+
   render() {
     return (
       <div>
-        Pokemon {this.state.loading ? "Loading":"Done"}
+        Pokemon
         <h1>{this.state.name}</h1>
       </div>
     );
